11c1b13b3d56ec7afabfd3e882e8967f7b3db5e4
maticrown <49392006+maticrown@users.noreply.github.com>
2020-10-30 12:14:31 +0200

    Update noun.pl

--- algorithm/dictionary/noun.pl
+++ algorithm/dictionary/noun.pl
@@ -1,15 +1,5 @@
 n(LF)     --> [N], {n(N, LF)}.
-/*
-n(author,     X^ author(X)).
-n(book,       X^ book(X)).
-n(professor,  X^ professor(X)).
-n(program,    X^ program(X)).
-n(programmer, X^ programmer(X)).
-n(student,    X^ student(X)).
-n(person,     X^ person(X)).
-n(language,   X^ language(X)).
-*/
-%%% added
+
 n(monarche,   X^ monarche(X)).
 n(money,      X^ money(X)).
 n(acting_royal, X^ acting_royal(X)).
@@ -19,34 +9,12 @@ n(female_,    X^ female_(X)).
 n(prince_,    X^ prince_(X)).
 n(princess_,  X^ princess_(X)).
 
-%%%
 n(parent_, X^ parent_(X)).
 n(father_, X^ father_(X)).
 n(mother_, X^ mother_(X)).
-%%%
-
-%%%
 
 pn((E^S)^S) --> [PN], {pn(PN, E)}.
-/*
-pn(allen,    allen).
-pn(bruce,    bruce).
-pn(bertrand, bertrand).
-pn(terry,    terry).
-pn(bill,     bill).
-pn(david,    david).
-pn(kathy,    kathy).
-pn(behshad,  behshad).
-pn(shane,    shane).
-pn(principia, principia).
-pn(shrdlu,   shrdlu).
-pn(prolog,   prolog).
-pn(english,  english).
-pn(chinese,  chinese).
-pn(korean,   korean).
-pn(swahili,  swahili).
-*/
-%% added
+
 pn(anne, anne).
 pn(camilla, camilla).
 pn(diana, diana).
@@ -93,4 +61,3 @@ pn(viscount, viscount).
 pn(andrew, andrew).
 pn(antony, antony).
 pn(pound, pound).
-%%
